ab78a4935bdfe62503d4707e957ef6ecd68920fc
Rahmat <rahmatbola3@gmail.com>
2023-11-29 07:13:32 -0800

    task 7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(9):
     for j in range(i + 1, 10):
-        print("{:02d}, {:02d}".format(i, j), end=", ")
+        print("{:02d}".format(j), end=", ")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def islower(c):
+    # check if the character is within the range of lowercase letters
+    return ord('a') <= ord(c) <= ord('z')
