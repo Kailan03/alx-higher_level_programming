1476a0e93d25c915a27168bac4f6b94303a4f93b
Rahmat <rahmatbola3@gmail.com>
2023-11-28 17:40:05 -0800

    task 0

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10, 10)
+number = random.randint(-1000, 1000)
 # extract the last digit of the number
 lastDigit = number % 10
 # print the required output
-print(lastDigit, end=" " )
+print(lastDigit, end=" ")
 if lastDigit > 0:
     print("is positive")
 elif lastDigit < 0:
