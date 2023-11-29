0e2f659898314eff1a3b06467c88f4a7cb8fcbf2
Rahmat <rahmatbola3@gmail.com>
2023-11-28 16:53:56 -0800

    task 0

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+#extract the last digit of the number
+lastDigit = abs(number) % 10
+#print the required output
+print("Last digit of", number, "is", lastDigit, "and is", end=" ")
+if lastDigit > 5:
+    print("greater than 5")
+elif lastDigit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
