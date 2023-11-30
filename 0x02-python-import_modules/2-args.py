d8e2f92b9622d0b05df2e93e56dbcb309b230646
Rahmat <rahmatbola3@gmail.com>
2023-11-30 12:53:45 -0800

    module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     numArgs = len(argv) - 1  # Subtract 1 for the script name
     arg_plural = "arguments" if numArgs != 1 else "argument"
     sep = ":" if numArgs > 0 else "."
+    endLine = "" if numArgs > 0 else ""
 
-    print("{} {}{}{}".format(numArgs, arg_plural, sep, "" if numArgs > 0 else ""))
+    print("{} {}{}{}".format(numArgs, arg_plural, sep, endLine))
 
     for i in range(1, numArgs + 1):
         print("{}: {}".format(i, argv[i]))
