c9722a0480c628ade590b11eea69c780d82b8b50
Rahmat <rahmatbola3@gmail.com>
2023-11-29 20:39:52 -0800

    fizzbuzz

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -3,6 +3,7 @@ def pow(a, b):
    if b == 0:
     return 1
 result = 1
-for _ in range(b):
-    result *= a
-return result
+# Use a loop to multiply a by itself b times
+        for _ in range(b):
+            result *= a
+            return result
