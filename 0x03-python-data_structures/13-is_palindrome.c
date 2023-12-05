0ecc37438efe833c068e10bb0790195a8963d8c8
Rahmat <rahmatbola3@gmail.com>
2023-12-04 19:08:19 -0800

    Technical interview prep

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,28 +11,28 @@
 int is_palindrome(listint_t **head);
 int is_palindrome(listint_t **head)
 {
-        if (*head == NULL || (*head)->next == NULL)
-                return (1);  /* An empty list or a list with one element is a palindrome*/
-        listint_t *slow_ptr = *head;
-        listint_t *fast_ptr = *head;
-        listint_t *second_half;
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);  /* An empty list or a list with one element is a palindrome*/
+	listint_t *slow_ptr = *head;
+	listint_t *fast_ptr = *head;
+	listint_t *second_half;
 
     /* Move slow_ptr to the middle of the list*/
-        while (fast_ptr != NULL && fast_ptr->next != NULL)
-        {
-                fast_ptr = fast_ptr->next->next;
-                slow_ptr = slow_ptr->next;
-        }
+	while (fast_ptr != NULL && fast_ptr->next != NULL)
+	{
+		fast_ptr = fast_ptr->next->next;
+		slow_ptr = slow_ptr->next;
+	}
 
     /* If the length of the list is odd, move slow_ptr to the next node*/
-        if (fast_ptr != NULL)
-        {
-                slow_ptr = slow_ptr->next;
-        }
+	if (fast_ptr != NULL)
+	{
+		slow_ptr = slow_ptr->next;
+	}
 
     /* Reverse the second half of the list*/
-        second_half = reverse_list(slow_ptr);
+	second_half = reverse_list(slow_ptr);
 
     /* Compare the first half and the reversed second half*/
-        return (compare_lists(*head, second_half));
+	return (compare_lists(*head, second_half));
 }
