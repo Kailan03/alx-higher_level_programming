1954c71884628dd1f973206cef8731da3b2920cf
Rahmat <rahmatbola3@gmail.com>
2023-12-19 16:34:43 -0800

    corrections

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+""" This is an empty class square
+
+It defines a square
+"""
+class Square:
 """ This class represents a square.
 
 It describes an empty square class
 """
 
-
-class Square:
     pass
