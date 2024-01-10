7120719a36a212b097fb1ae9037373f608df893d
Rahmat <rahmatbola3@gmail.com>
2024-01-10 10:38:27 -0800

    1&2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+defines a funcion that appends a string at the end of a file
+and returns number of character added
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a str to a text file and returns the number of characters written.
+
+    Parameters:
+    - filename (str): The name of the file to be written.
+    - text (str): The string to be written to the file.
+
+    Returns:
+    - int: The number of characters written to the file.
+
+    Example:
+    >>> write_file("output.txt", "Hello, World!")
+    13
+
+    Additional Tests:
+    >>> write_file("existing_file.txt", "New content.")
+    # Overwriting an existing file
+    # Output: 12
+
+    >>> write_file("new_file.txt", "")
+    # Writing an empty string
+    # Output: 0
+    """
+
+    try:
+        with open(filename, mode="w", encoding="utf-8") as file:
+            file.write(text)
+            return len(text)
+    except TypeError:
+        print("Invalid filename or text type. Please provide valid strings.")
+        return 0
