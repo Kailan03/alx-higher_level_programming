e9c9b79f5c3b1e9df6695398bc19d23427bda3d5
Rahmat <rahmatbola3@gmail.com>
2024-03-14 04:30:19 +0000

    10.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
 
 // Define the function converter
-exports.converter = function(base) {
+exports.converter = function (base) {
   // Recursive function to convert number to specified base
-  return function convert(num) {
-    // Base case: if num is 0, return an empty string
-    return num === 0 ? '' : convert(Math.floor(num / base)) + '0123456789abcdefghijklmnopqrstuvwxyz'.charAt(num % base);
-  }
-}
+  return function convert (num) {
+    // Base case: if num is 0, return '0'
+    if (num === 0) {
+      return '0';
+    }
+    // Otherwise, convert the number to the specified base
+    let result = '';
+    while (num > 0) {
+      result = '0123456789abcdefghijklmnopqrstuvwxyz'.charAt(num % base) + result;
+      num = Math.floor(num / base);
+    }
+    return result;
+  };
+};
